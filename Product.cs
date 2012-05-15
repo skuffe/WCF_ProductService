d5ce70581e2369aa2dd8364193e944c470ef0461
Martin Mortensen <msmortensen@gmail.com>
2012-05-15 09:10:26 +0200

    Initial import

---
+++ Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace WCFandEFService
+{
+	// Use a data contract as illustrated in the sample below to add composite types to service operations
+	[DataContract]
+	public class Product
+	{
+		[DataMember]
+		public int ProductID { get; set; }
+		[DataMember]
+		public string ProductName { get; set; }
+		[DataMember]
+		public string QuantityPerUnit { get; set; }
+		[DataMember]
+		public decimal UnitPrice { get; set; }
+		[DataMember]
+		public bool? Discontinued { get; set; }
+	}
+}
