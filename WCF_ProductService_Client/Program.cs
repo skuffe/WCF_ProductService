bbd1f960aa4e4c0579d19a9a061f46a196830589
Danni Skov Hoeglund <skuffe@pwnz.dk>
2012-05-15 10:24:59 +0200

    - Filetree restructured to include solution files and client project

---
+++ WCF_ProductService_Client/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WCF_ProductService_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
